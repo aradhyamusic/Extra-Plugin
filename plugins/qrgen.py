98cf36b4d0ce0aebffca6d98e1682e0d06e15ff9
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-19 10:50:56 +0530

    Update qrgen.py

--- plugins/qrgen.py
+++ plugins/qrgen.py
@@ -9,7 +9,7 @@ async def write_text(client, message):
         await message.reply_text("**Usage**:- `/qr https://t.me/rishu1286`")
         return
     text = " ".join(message.command[1:])
-    photo_url = "https://apis.xditya.me/qr/gen?text=" + text
+    photo_url = "https://dynamicqrcode.nepcoderdevs.workers.dev/?message=" + text
     await app.send_photo(
         chat_id=message.chat.id, photo=photo_url, caption="Here is your qrcode"
     )
